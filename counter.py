6e90487a1f75d3d0bba26520fe497d252ee7ff86
khrastnig <victy@gmx.at>
2022-06-13 08:10:37 +0200

    Abgabe

--- counter.py
+++ counter.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 # 2021 nr@bulme.at
+#Karoline Hrastnig
+#12.10.2022
+#Binary Counter
 
-from gpiozero import Button
+from gpiozero import Button, LED, LEDBoard
 from PyQt5.QtCore import Qt, QObject, pyqtSignal
 from PyQt5.QtWidgets import (QWidget, QLCDNumber,
     QVBoxLayout, QApplication)
 
 DOWN_PIN = 22
 RESET_PIN = 27
-UP_PIN = 17 
+UP_PIN = 17
+leds=[LED(25),LED(24),LED(23),LED(18)]
 
 class QtButton(QObject):
-    changed = pyqtSignal()
+    changed = pyqtSignal()                              # funktion signal -> emit()
 
     def __init__(self, pin):
         super().__init__()
         self.button = Button(pin) 
-        self.button.when_pressed = self.gpioChange        
+        self.button.when_pressed = self.gpioChange      #rufe gpioChange auf wenn der Button gedrückt wird       
 
     def gpioChange(self):
-        self.changed.emit()
+        self.changed.emit()                             #Änderung wenn Signal 
 
 class Counter(QWidget):
     def __init__(self):
@@ -32,23 +36,47 @@ class Counter(QWidget):
         self.lcd.display(0)
 
         vbox = QVBoxLayout()
-        vbox.addWidget(self.lcd)
+        vbox.addWidget(self.lcd)                       #Anzeige
 
-        self.setLayout(vbox)
+        self.setLayout(vbox)                           #Anzeigefeld
         self.setMinimumSize(400, 200)
-        self.setWindowTitle('Counter')
+        self.setWindowTitle('Counter')                 #Beschriftung Anzeigefeld
         self.show()
-
-
+        
+    def setLed (self, count):                          #Funktion um die Leds anzusteuern
+        for i, led in enumerate(leds):
+            if (count & 1<<i):                
+                led.on()
+            else:                
+                led.off()          
+    
     def countUp(self):
+        if self.count == 15:
+             self.count = -1   
         self.count += 1
+        self.lcd.display(self.count)                    #Gibt den Wert in self.lcd.display aus also in der Anzeige
+        self.setLed(self.count)                         #Gibt den Wert in am Ledboard aus
+                
+    def countDown(self):
+        if self.count == 0:
+             self.count = 16
+        self.count -= 1
         self.lcd.display(self.count)
- 
-
+        self.setLed(self.count)
+                
+    def countReset(self):
+        self.count = 0
+        self.lcd.display(self.count)
+        self.setLed(self.count)
+        
 if __name__ ==  '__main__':
     app = QApplication([])
-    gui = Counter()
-    button = QtButton(UP_PIN)
-    button.changed.connect(gui.countUp)
+    gui = Counter()                                     #greift auf die Klasse Counter zu  
+    buttonUp = QtButton(UP_PIN)                         #bindet den Button UP_PIN aus der QtButtonklasse an buttonUp
+    buttonUp.changed.connect(gui.countUp)                #bindet buttonUp , bei Veränderung (Funktion changed) an die Funktion countUp aus der Counterklasse 
+    buttonDown = QtButton(DOWN_PIN)
+    buttonDown.changed.connect(gui.countDown)
+    buttonReset = QtButton(RESET_PIN)
+    buttonReset.changed.connect(gui.countReset)
     app.exec_()
     
